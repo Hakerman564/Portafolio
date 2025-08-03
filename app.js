b86e287bb8069216187652125ca88f3dc4f191fd
Negel <elbueno000sin@gmail.com>
2025-08-03 15:02:05 -0400

    Minimalismo 3 agosto

---
+++ app.js
@@ -0,0 +1,8 @@
+// Data Initial.
+
+Personalizacion =  {
+    perfil: "Tengo experiencia en diferente parte del desarrollo de software especialmente en el área de FRONTEND espacialmente en el en maquetado de Web y Web responsive, estoy en busca de nuevas oportunidades Profesionales, Y participar en proyecto creando soluciones.",
+    habilidadClaves: {
+        URL: 'icon/' 
+    } 
+}
