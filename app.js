b8e507fe540dd6f7b6998042f27e915befaa3e08
Negel <elbueno000sin@gmail.com>
2025-08-03 22:47:21 -0400

    Javascript funcionalidad de tabla de datos de rol

--- app.js
+++ app.js
@@ -1,8 +1,37 @@
 // Data Initial.
 
 Personalizacion =  {
+    Rol: "Negel Diaz: Desarrolador FullStack Jr",
     perfil: "Tengo experiencia en diferente parte del desarrollo de software especialmente en el área de FRONTEND espacialmente en el en maquetado de Web y Web responsive, estoy en busca de nuevas oportunidades Profesionales, Y participar en proyecto creando soluciones.",
     habilidadClaves: {
-        URL: 'icon/' 
-    } 
+        html5: "icon/html5.svg",
+        css: "icon/css.svg",
+        js: "icon/javascript.svg",
+        git: "icon/git.svg",
+        json: "icon/json.svg",
+        net: "icon/dotnet.svg",
+        vs: "icon/visualstudiocode.svg",
+        github: "icon/github.svg",
+        api: "icon/openapiinitiative.svg",
+        mysql: "icon/mysql.svg",
+        sqlserver: "icon/icons8-servidor-microsoft-sql-50"
+    }, 
+    
+    Habilidades_Rol:{
+        encabezado: "Capacidades de Desarrollo FullStack",
+        habilidad: {
+            Maquetacion:"Diseño y maquetacion css",
+            RestApi: "Desarrollo Rapido con Rest Api y Open-api",
+            Responsive: "Diseño web responsivo",
+            BaseDatos: "Diseño y gestion de base de datos",
+            Integracion: "Integracion Frontend-Backend"
+        }
+        
+    },
+
+    Habilidades_Core:{
+        Core: {
+            
+        }
+    }
 }
