4fd3f0b3950313e98d743488d8e9ebe55c45d8b7
Negel <elbueno000sin@gmail.com>
2025-08-04 23:06:09 -0400

    step perfil completado

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 // Data Initial.
 
-Personalizacion =  {
+
+/*
+
+
+
+
+Educación
+Educacion Formal
+Institucto técnico comunitario 2020 – 2022: técnico en desarrollo software
+Universidad católica de Santo Domingo 2023 – termino: Ingenieria en sistemas Computacionales.
+Eduacacion especializada
+talengin 2024- 2025  intelegencia artificial generativa.
+
+
+*/
+Personalizacion = {
     Rol: "Negel Diaz: Desarrolador FullStack Jr",
     perfil: "Tengo experiencia en diferente parte del desarrollo de software especialmente en el área de FRONTEND espacialmente en el en maquetado de Web y Web responsive, estoy en busca de nuevas oportunidades Profesionales, Y participar en proyecto creando soluciones.",
     habilidadClaves: {
@@ -15,23 +30,100 @@ Personalizacion =  {
         api: "icon/openapiinitiative.svg",
         mysql: "icon/mysql.svg",
         sqlserver: "icon/icons8-servidor-microsoft-sql-50"
-    }, 
-    
-    Habilidades_Rol:{
+    },
+
+    Habilidades_Rol: {
         encabezado: "Capacidades de Desarrollo FullStack",
         habilidad: {
-            Maquetacion:"Diseño y maquetacion css",
+            Maquetacion: "Diseño y maquetacion css",
             RestApi: "Desarrollo Rapido con Rest Api y Open-api",
             Responsive: "Diseño web responsivo",
             BaseDatos: "Diseño y gestion de base de datos",
             Integracion: "Integracion Frontend-Backend"
         }
-        
+
     },
 
-    Habilidades_Core:{
-        Core: {
-            
+    Habilidades_Core: {
+
+        PensamientoCritico: {
+            encabezado: "Pensamiento critico",
+            icon_url: "",
+            contedio: "",
+
+        },
+
+          AutoDidacta: {
+            encabezado: "autoDidacta",
+            icon_url: "",
+            contedio: "",
+
+        },
+
+          MejoraContinua: {
+            encabezado: "Mejora Continua",
+            icon_url: "",
+            contedio: "",
+
+        },
+
+          TrabajoEquipo: {
+            encabezado: "Trabajo en equipo",
+            icon_url: "",
+            contedio: "",
+
+        },
+
+     
+
+    },
+
+    EduacionFormal:
+    {
+        ITSC:
+        {
+            Nombre: "Institucto Tecnico Superior Comunitario",
+            Acronimo: "ITSC",
+            Temporada: "2020-2022",
+            Titulo: "Tecnico Profecional En desarrollo Software",
+            Estatus: "Terminado"
+        },
+         UCSD:
+        {
+            Nombre: "Universidad católica de Santo Domingo",
+            Acronimo: "UCSD",
+            Temporada: {inicio: 2023},
+            Titulo: "TIngenieria en sistemas Computacionales",
+            Estatus: "Tesis"
+        },
+
+        BIA:{
+             Nombre: "Telengin",
+            Acronimo: "Bia",
+            Temporada: "2024-2025",
+            Titulo: "Boostcamp en Inteligencia Artificial Generativa",
+            Estatus: "Terminado"
         }
+    },
+    Certificado:
+    {
+        Htlm5Url: ""
+    },
+
+    Proyectos:{
+        Contenido :"Explora algunos de mis trabajos más relevantes, donde aplico mis habilidades para resolver problemas y crear soluciones innovadoras.",
+        GrandesProyecto:{
+            P1:{
+                encabezado:"",
+                contenido: "",
+                link: "",
+            }
+        }
+    },
+
+    miniProyectos:{
+        p1Url: ""
     }
+
+
 }
